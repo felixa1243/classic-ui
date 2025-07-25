5b4e6afd0de85509717e62fd3e3e415cbb325569
Rajiph iqbal <felixarajiph@gmail.com>
2025-07-26 00:28:01 +0700

    feat: add dropdown and alpine data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Alpine from "alpinejs";
 import { focus } from "@alpinejs/focus";
-import 'iconify-icon';
+import { persist } from "@alpinejs/persist";
 import './style.css';
 window.Alpine = Alpine;
 Alpine.plugin(focus);
+Alpine.plugin(persist);
+window.addEventListener('alpine:init', () => {
+    Alpine.data('genericData', () => ({
+        tabOpened: Alpine.$persist('buttons'),
+        modalOpen: false,
+        modalType: null,
+        modaData: null,
+        setActiveTab(tab) {
+            this.tabOpened = tab
+        },
+        openModal(modalType, modalData) {
+            this.modalOpen = true;
+            this.modalType = modalType;
+            this.modalData = modalData;
+        },
+        closeModal() {
+            this.modalOpen = false
+        }
+    }))
+})
 Alpine.start();
