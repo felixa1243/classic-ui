0d370d4855b43181ae6ed179bf3be3e5ceed9957
Rajiph iqbal <felixarajiph@gmail.com>
2025-07-28 13:52:52 +0700

    feat: make modal modularized

--- src/main.js
+++ src/main.js
@@ -12,47 +12,28 @@ window.addEventListener('alpine:init', () => {
         tabOpened: Alpine.$persist('buttons'),
 
     }))
-    Alpine.store('popUpState', {
+    Alpine.store('modal', {
         modalOpen: false,
-        modalType: null,
+        modalHeader: null,
+        modalBody: null,
+        modalFooter: null,
         modalData: {},
         setActiveTab(tab) {
             this.tabOpened = tab
         },
-        openModal(modalType, modalData) {
+        openModal(headerID = 'default-modal-header', bodyID = 'default-modal-body', footerID = 'default-modal-footer') {
+            const headerTemplate = document.querySelector('#' + headerID);
+            const bodyTemplate = document.querySelector('#' + bodyID);
+            const footerTemplate = document.querySelector('#' + footerID);
+            this.modalHeader = headerTemplate?.content?.cloneNode(true).firstElementChild?.outerHTML || '';
+            this.modalBody = bodyTemplate?.content?.cloneNode(true).firstElementChild?.outerHTML || '';
+            this.modalFooter = footerTemplate?.content?.cloneNode(true).firstElementChild?.outerHTML || '';
             this.modalOpen = true;
-            this.modalType = modalType;
-            this.modalData = modalData;
         },
         closeModal() {
             this.modalOpen = false
         }
     })
-    Alpine.data('confirmModal', () => ({
-        modal: Alpine.store('popUpState'),
-        modalData: {},
-        onClick() {
-            this.modal.openModal('CONFIRM_MODAL', this)
-        },
-        title: 'This is Confirm Modal',
-        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor felis quis arcu consequat, vitae eleifend turpis euismod. Vivamus elementum lacus quam, at sagittis libero dictum sit amet. Praesent maximus purus et sollicitudin porttitor. Mauris augue magna, bibendum vel luctus et, sodales eget sapien. Cras cursus aliquet lorem. Fusce ultricies orci sit amet mi pharetra, condimentum vestibulum nunc mattis. Suspendisse ornare urna sapien, et condimentum tellus consectetur eget. Curabitur tristique convallis est varius iaculis. Donec lacinia lacinia nisi, eu varius dolor. Curabitur posuere ipsum et arcu commodo, eget rutrum dui pretium. Cras dapibus lectus suscipit, malesuada erat sed, consequat augue. Phasellus vitae finibus leo. Maecenas sollicitudin pretium egestas. Donec ut placerat justo, vel finibus nunc.
-
-Duis tellus orci, tincidunt eget nulla in, elementum aliquet nibh. Nunc venenatis neque sed nunc lacinia, non finibus nisl interdum. Maecenas viverra laoreet leo a consectetur. Quisque eget risus viverra, dapibus erat et, accumsan felis. Morbi posuere, orci vitae tempor posuere, justo libero volutpat magna, sit amet vestibulum odio turpis vitae nisl. Ut leo neque, maximus fermentum ex ac, dapibus imperdiet lectus. Etiam at tincidunt dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur at placerat elit. Donec consectetur consequat sapien efficitur pulvinar. Nullam bibendum magna eu neque fringilla, nec tincidunt lorem vestibulum. Suspendisse tristique euismod tincidunt. Integer facilisis convallis luctus.
-
-Mauris fringilla dolor eu neque elementum, vel sollicitudin lacus rhoncus. Suspendisse purus eros, porta non ex et, congue efficitur nisi. Pellentesque iaculis porta augue. Morbi sollicitudin, felis vitae interdum condimentum, enim eros feugiat est, id viverra purus ligula a lorem. Praesent quis laoreet est. Duis est ante, lacinia id placerat et, facilisis sit amet mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam vehicula nisl dapibus diam ullamcorper, vitae laoreet mauris aliquet. Sed faucibus eu ex a suscipit. In nibh lorem, imperdiet eu tincidunt et, tempus ac mauris. Donec luctus vitae augue vitae auctor. Ut vehicula dignissim sem non ultricies. Sed ut sapien vitae ex tempor iaculis. In a lectus arcu. Maecenas ac turpis tincidunt velit volutpat rhoncus. Praesent at leo interdum, posuere mauris ullamcorper, mattis purus.
-
-Morbi quis turpis velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vehicula lectus eu augue laoreet sollicitudin. Donec eleifend leo nunc. Proin et varius leo. Integer tristique finibus ante, vitae bibendum nisl placerat nec. Duis justo elit, pharetra eget magna eget, condimentum suscipit felis.
-
-Praesent pulvinar tellus eget tincidunt ultricies. Praesent vitae enim pretium, eleifend metus ut, maximus enim. Donec pulvinar aliquet neque sed ullamcorper. Morbi ultricies feugiat magna eu maximus. Donec tempor urna quis hendrerit elementum. Sed vel tincidunt lorem, sed vehicula ipsum. Quisque ut risus pharetra, elementum dui nec, aliquet elit. Pellentesque sed faucibus nunc, in sollicitudin tellus. Vestibulum finibus bibendum sem non facilisis. Vivamus non blandit eros. In sagittis urna urna, at vehicula est euismod vitae. Sed consequat eu sapien at elementum. Integer et feugiat quam.`,
-        onCancel() {
-            console.log('Canceled');
-            Alpine.store('popUpState').closeModal();
-        },
-        onConfirm() {
-            console.log('Confirmed');
-            Alpine.store('popUpState').closeModal();
-        }
-    }))
     function createCodeComponent({ templateId, withToggle = false, withCopy = false }) {
         return {
             toggle: 'Display',
